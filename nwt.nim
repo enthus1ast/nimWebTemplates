4edcacd54c5104109d6659bfdcb1574fa4994d40
enthus1ast <enthus1ast>
2017-06-04 17:51:55 +0200

    nwtTokenizer ausgelagert
    Signed-off-by: enthus1ast <enthus1ast>

--- nwt.nim
+++ nwt.nim
@@ -26,116 +26,13 @@ import sequtils
 import os
 import tables
 import commandParser
+import nwtTokenizer
 
-type 
-  TemplateSyntaxError = ref object # of Exception
-  UnknownTemplate = ref object  #of Exception
-
-  NwtToken = enum
-    NwtString, # a string block
-    NwtComment,
-    NwtEval,
-    NwtVariable
-
-  Token = object of RootObj 
-    tokenType: NwtToken # the type of the token
-    value: string # the value 
-
+type
   Nwt = ref object of RootObj
     # templates*: StringTableRef ## we load all the templates we want to render to this strtab
     templates*: Table[string,seq[Token]] ## we parse templates on start
     templatesDir*: string
-
-  Block = tuple[name: string, posStart: int, posEnd: int]
-
-
-proc newToken(tokenType:NwtToken, value: string): Token = 
-  result = Token()
-  result.tokenType = tokenType
-  result.value = value
-
-proc debugPrint(buffer: string, pos: int) = 
-  var pointPos = if pos - 1 < 0: 0 else: pos - 1
-  echo buffer
-  echo '-'.repeat(pointPos) & "^"
-
-iterator nwtTokenize(s: string): Token =
-  ## transforms nwt templates into tokens
-  var 
-    buffer: string = s 
-    pos = 0
-    toyieldlater = "" # we use this to reconstruct a string whitch contains a "{"
-
-  while true:
-    var stringToken = ""
-    pos = buffer.parseUntil(stringToken,'{',pos) + pos
-    # buffer.debugPrint(pos)
-
-    if buffer == "{":
-      # echo "buffer ist just '{'"
-      yield newToken(NwtString, "{")
-      break
-
-    if stringToken.len == buffer.len:
-      # echo "we have read the string at once! no '{' found"
-      yield newToken(NwtString, stringToken)
-      break
-
-    if stringToken != "" :
-      toyieldlater.add stringToken
-    pos.inc # skip "{"
-    if buffer.continuesWith("{",pos): 
-      if toyieldlater != "": yield newToken(NwtString, toyieldlater); toyieldlater = ""
-      pos.inc # skip {
-      pos = buffer.parseUntil(stringToken,'}',pos) + pos
-      yield newToken(NwtVariable, stringToken.strip())
-      pos.inc # skip }
-      pos.inc # skip }
-    elif buffer.continuesWith("#",pos): 
-      if toyieldlater != "": yield newToken(NwtString, toyieldlater); toyieldlater = ""
-      pos.inc # skip #
-      pos = buffer.parseUntil(stringToken,'#',pos) + pos
-      pos.inc # skip end # 
-      if buffer.continuesWith("}", pos): 
-        pos.inc # skip }
-        yield newToken(NwtComment, stringToken[0..^1].strip())
-    elif buffer.continuesWith("%",pos): 
-      if toyieldlater != "": yield newToken(NwtString, toyieldlater); toyieldlater = ""
-      pos.inc # skip #
-      pos = buffer.parseUntil(stringToken,'%',pos) + pos
-      pos.inc # skip end # 
-      if buffer.continuesWith("}", pos): 
-        pos.inc # skip }
-        yield newToken(NwtEval, stringToken[0..^1].strip())
-    else:
-      if pos >= buffer.len:
-        # echo "we have reached the end of buffer"
-        yield newToken(NwtString, toyieldlater)
-      else:
-        # echo "we found a { somewhere so we have to prepend it"
-        toyieldlater = toyieldlater & "{" 
-      discard
-
-    if pos >= buffer.len: # TODO check if this has to be '>'
-      ## Nothing to do for us here
-      break
-
-proc toStr(token: Token, params: StringTableRef = newStringTable()): string = 
-  ## transforms the token to its string representation 
-  # TODO should this be `$`?
-  case token.tokenType
-  of NwtString:
-    return token.value
-  of NwtComment:
-    return ""
-  of NwtVariable:
-    var bufval = params.getOrDefault(token.value)
-    if bufval == "":
-      return "{{" & token.value & "}}" ## return the token when it could not replaced
-    else:
-      return bufval
-  else:
-    return ""
 
 proc add*(tokens: var Table[string,seq[Token]], templateName, templateStr: string) =
   ## parses and adds/updates a template
@@ -164,23 +61,6 @@ proc newNwt*(templatesDir: string = "./templates/*.html"): Nwt =
   result.templatesDir = templatesDir
   result.loadTemplates(templatesDir)
 
-proc extractTemplateName(raw: string): string = 
-  ## returns the template name from
-  ##  extends "base.html"
-  ## returns "base.html"
-  var parts = raw.strip().split(" ")
-  if parts.len != 2:
-    # TemplateSyntaxError
-    raise newException(ValueError, "Could not extract template name from '$1'" % [raw])
-    # raise (OsError, "Could not extract template name from '$1'" % [raw])
-  result = parts[1].captureBetween('"', '"')
-  if result != "": return
-
-  result = parts[1].captureBetween('\'', '\'')
-  if result != "": return
-
-  result = parts[1] #   " or ' are missing
-  
 proc getBlocks(tokens: seq[Token]): Table[string, Block] =
   # returns all {%block 'foo'%} statements as a Table of Block
   result = initTable[string, Block]()
@@ -283,56 +163,6 @@ when isMainModule:
   # var nwt = newNwt()
   # echo "Loaded $1 templates." % [$nwt.templates.len]
 
-  ## Tokenize tests
-  assert toSeq(nwtTokenize("hello")) == @[newToken(NwtString, "hello")]
-  assert toSeq(nwtTokenize("{{var}}")) == @[newToken(NwtVariable, "var")]
-  assert toSeq(nwtTokenize("{{ var }}")) == @[newToken(NwtVariable, "var")]
-  assert toSeq(nwtTokenize("{{var}}{{var}}")) == @[newToken(NwtVariable, "var"),newToken(NwtVariable, "var")]
-  assert toSeq(nwtTokenize("{#i am a comment#}")) == @[newToken(NwtComment, "i am a comment")]
-  assert toSeq(nwtTokenize("{# i am a comment #}")) == @[newToken(NwtComment, "i am a comment")]
-  assert toSeq(nwtTokenize("{%raw%}")) == @[newToken(NwtEval, "raw")]
-  assert toSeq(nwtTokenize("{% raw %}")) == @[newToken(NwtEval, "raw")]
-  assert toSeq(nwtTokenize("{% for each in foo %}")) == @[newToken(NwtEval, "for each in foo")]
-
-  assert toSeq(nwtTokenize("body { background-color: blue; }")) == 
-    @[newToken(NwtString, "body { background-color: blue; }")]
-
-  assert toSeq(nwtTokenize("{ nope }")) == @[newToken(NwtString, "{ nope }")]
-  assert toSeq(nwtTokenize("{nope}")) == @[newToken(NwtString, "{nope}")]
-  assert toSeq(nwtTokenize("{nope")) == @[newToken(NwtString, "{nope")]
-  assert toSeq(nwtTokenize("nope}")) == @[newToken(NwtString, "nope}")]
-
-  assert toSeq(nwtTokenize("{")) == @[newToken(NwtString, "{")]
-  assert toSeq(nwtTokenize("}")) == @[newToken(NwtString, "}")]
-
-  assert toSeq(nwtTokenize("""{%block 'first'%}{%blockend%}""")) == @[newToken(NwtEval, "block 'first'"), newToken(NwtEval, "blockend")]
-  assert toSeq(nwtTokenize("foo {baa}")) == @[newToken(NwtString, "foo {baa}")]
-
-  assert toSeq(nwtTokenize("foo {{baa}} {baa}")) == @[newToken(NwtString, "foo "), 
-                                                      newToken(NwtVariable, "baa"),
-                                                      newToken(NwtString, " {baa}")]
-  ## extractTemplateName tests
-  assert extractTemplateName("""extends "foobaa.html" """) == "foobaa.html"
-  assert extractTemplateName("""extends "foobaa.html"""") == "foobaa.html"
-  assert extractTemplateName("""extends 'foobaa.html'""") == "foobaa.html"
-  assert extractTemplateName("""extends foobaa.html""") == "foobaa.html"
-  assert extractTemplateName("""extends foobaa.html""") == "foobaa.html" 
-  assert extractTemplateName(toSeq(nwtTokenize("""{% extends "foobaa.html" %}"""))[0].value) == "foobaa.html" 
-  block: 
-    var tokens = toSeq(nwtTokenize("""{% extends "foobaa.html" %}{% extends "goo.html" %} """)) 
-    assert extractTemplateName(tokens[0].value) == "foobaa.html"
-    assert extractTemplateName(tokens[1].value) == "goo.html"
-  block: 
-    var tokens = toSeq(nwtTokenize("""{% extends foobaa.html %}{% extends goo.html %}""")) 
-    assert extractTemplateName(tokens[0].value) == "foobaa.html"
-    assert extractTemplateName(tokens[1].value) == "goo.html"
-  block: 
-    var tokens = toSeq(nwtTokenize("""{%extends "foobaa.html" %}{% extends 'goo.html' %}""")) 
-    assert extractTemplateName(tokens[0].value) == "foobaa.html"
-    assert extractTemplateName(tokens[1].value) == "goo.html"
-  block: 
-    var tokens = toSeq(nwtTokenize("""{%extends foobaa.html%}""")) 
-    assert extractTemplateName(tokens[0].value) == "foobaa.html"
 
 
   block:
