71ccb8261cdc5e7b7cc8320f71b24504e8ad8a74
enthus1ast <enthus1ast>
2017-06-05 16:00:50 +0200

    Signed-off-by: enthus1ast <enthus1ast>

--- ex.nim
+++ ex.nim
@@ -7,15 +7,15 @@ import asyncdispatch
 # import json
 import tables
 
-var t = newNwt("templates/*.html")
+var t = newNwt("templates/*")
 # var db = newFlatDb("tst.db", false)
 # if db.nodes.len == 0:
   # discard db.append(%* {"site": "index.html", "content": "ich bin ein bisschen content"})
 routes:
   get "/":
     resp t.renderTemplate("index.html", newStringTable({"content": $epochTime()}))
-  get "/@name":
-    resp t.renderTemplate(@"name" & ".html", newStringTable({"content": $epochTime()}))    
+  get "/@name":#& ".html"
+    resp t.renderTemplate(@"name" , newStringTable({"content": $epochTime()}))    
     # resp t.renderTemplate("index.html",  newStringTable(db.queryOne equal("site","index.html")) ))
   # get "/ass":
   #   resp t.renderTemplate("ass.html", newStringTable({"content": $epochTime()}))
