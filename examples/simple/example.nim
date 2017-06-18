2d094d11e01691c1ac19345e6357936d7436dac5
enthus1ast <enthus1ast>
2017-06-18 03:05:40 +0200

    Signed-off-by: enthus1ast <enthus1ast>

--- examples/simple/example.nim
+++ examples/simple/example.nim
@@ -6,13 +6,15 @@ var templates = newNwt("templates/*.html") # we have all the templates in a fold
 
 var server = newAsyncHttpServer()
 proc cb(req: Request) {.async.} =
-  let res = req.url.path #.strip(leading=true, trailing=false, {'/'})
+  let res = req.url.path
 
   case res 
   of "/", "/index.html":
-    await req.respond(Http200, templates.renderTemplate("index.html") )  
+    await req.respond(Http200, templates.renderTemplate("index.html"))  
   of "/about.html":
     await req.respond(Http200, templates.renderTemplate("about.html"))
+  of "/stats.html":
+    await req.respond(Http200, templates.renderTemplate("stats.html", %* {"title": "some variables from nim", "foo": "Foo!", "baa": "Baa!"}))  
   else:
     await req.respond(Http404, "not found")
 
