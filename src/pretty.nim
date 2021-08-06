494e3bd49ab2c7d9c10decc8a5eff4d1d885b341
David Krause <krause@biochem2.uni-frankfurt.de>
2021-08-06 10:46:40 +0200

    Signed-off-by: David Krause <krause@biochem2.uni-frankfurt.de>

--- src/pretty.nim
+++ src/pretty.nim
@@ -1,13 +1,13 @@
 import parser, nwtTokenizer
 import json, sequtils
 
-proc prettyNwt*(str: string): string =
+proc prettyNwt*(str: string): string {.compileTime.} =
   ## pretty print parsed nwt template (as json)
   var lexerTokens = toSeq(nwtTokenize(str))
   var firstStepTokens = parseFirstStep(lexerTokens)
   var pos = 0
   var secondsStepTokens = parseSecondStep(firstStepTokens, pos)
-  return (%* secondsStepTokens).pretty()
+  (%* secondsStepTokens).pretty()
 
 
 when isMainModule:
